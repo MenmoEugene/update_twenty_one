b07fe11f6a3cf752123c6c95738d10fb5234c3a2
MenmoEugene <menmo_o@163.com>
2017-12-21 22:40:22 +0800

    scdm

---
+++ IntegerDemo.java
@@ -0,0 +1,36 @@
+class IntegerDemo
+{
+	public static void main(String[] args)
+	{
+		Integer x = 4;//自动装箱  new Integer(4);
+		
+		x = x +2;//自动拆箱  x+2 ：进行自动拆箱，变成了int类型，和2进行加法运算，再将和进行装箱赋给x。
+		
+		Integer m = 128;
+		Integer n = 128;
+		
+		sop("m==n:"+(m==n));
+		
+		Integer a = 127;
+		Integer b = 127;
+		
+		sop("a==b:"+(a==b));//结果为true，因为a 和 b指向了同一个Integer对象。
+						   //因为当数值在byte范围内容，对于新特性，
+						   //如果该数值已经存在，则不会再开辟新的空间。
+	}
+	
+	public static void method()
+	{
+		Integer x = new Integer("123");
+		
+		Integer y = new Integer(123);
+		
+		sop("x==y"+(x==y));
+		sop("x.equals(y)"+x.equals(y));
+	}
+	public static void sop(String str)
+	{
+		System.out.println(str);
+	}
+}
+
